6a796cc3e6b8fe46f40f410ad571897fcc17c4a4
Tanya <zzabelinka@gmail.com>
2015-04-28 23:45:41 +0400

    Newton method works correctly. Counts the number of arithmetic operations. Improved function LU

--- LUsolveSLAU.m
+++ LUsolveSLAU.m
@@ -1,27 +1,22 @@
-function [x, counter] = LUsolveSLAU (A, b, L, U, P)
-
-%[L, U, P] = lu(A);
-counter = 0;
-%переставляем элементы в векторе
-x = P*b;
+function [x, counter] = LUsolveSLAU (A, b, L, U, P) % возвращает решение СЛАУ A * x = bс помощью известного LUP - разложения
+counter = 0;        %счетчик произведенных арифметических операций
+x = P * b;      %переставляем элементы в векторе
 %преобразования с помощью L -- прямой ход
-for k = 1:size(b)-1
-    for i = k+1:size(b)
-        x(i) = x(i) - x(k)*L(i, k);
+for k = 1 : size(b) - 1
+    for i = k + 1 : size(b)
+        x(i) = x(i) - x(k) * L(i, k);
         counter = counter + 2;
     end
 end
 %получаем единицы на диагонали
-x = x./diag(U); 
+x = x ./ diag(U); 
 counter = counter + 1;
 %обратный ход
-for k = size(b):-1:2
-    for i = k-1:-1:1
-        if U(i, k)~= 0
-            x(i) = x(i) - x(k)*U(i, k)/U(i, i);
+for j = size(b): -1 :2
+    for i = j-1:-1:1
+        if U(i, j) ~= 0
+            x(i) = x(i) - x(j) * U(i, j) / U(i, i);
             counter = counter + 3;
         end
     end
 end
-disp(' При решении линейной системы произведено операций ')
-disp(counter)
--- Newton10.m
+++ Newton10.m
@@ -1,12 +1,13 @@
-xn = [0.5; 0.5; 1.5; -1.0; -0.5; 1.5; 0.5; -0.5; 1.5; -1.5];
+xn = [0.5; 0.5; 1.5; -1.0; -0.5; 1.5; 0.5; -0.5; 1.5; -1.5];        %начальное приближение
 J = setMatrixJ(xn)       %матрица частных производных в точке xn
 y = setVectorF(xn)'      %вектор значений функций в точке xn
-[L, U, P] = myLU(J);
-delta = LUsolveSLAU(J, -y, L, U, P) 
+[L, U, P, counter] = myLU(J);    %делаем разложение, считаем количество произведенных операций
+globalCounter = counter;        %количество арифметических операций, произведенных в методе Ньютона
+[delta, counter] = LUsolveSLAU(J, -y, L, U, P)  %решаем СЛАУ, считаем операции
+globalCounter = globalCounter + counter;        %количество арифметических операций, произведенных в методе Ньютона
 i = 0;                   %счетчик номера итерации
 while max(abs(delta)) > 1e-5 & i < 100
     xn = xn + delta;        %переход к следующему приближению
-    
     i = i + 1;
     disp('Итерация номер ')
     disp(i)
@@ -17,10 +18,14 @@ while max(abs(delta)) > 1e-5 & i < 100
     %пересчитываем для нового xn
     J = setMatrixJ(xn);      %матрица частных производных в точке xn
     y = setVectorF(xn)';     %вектор значений функций в точке xn
-   	[L, U, P] = myLU(J);
-    delta = LUsolveSLAU(J, -y, L, U, P) 
+   	[L, U, P, counter] = myLU(J);
+    globalCounter = globalCounter + counter;        %количество арифметических операций, произведенных в методе Ньютона
+    [delta, counter] = LUsolveSLAU(J, -y, L, U, P) 
+    globalCounter = globalCounter + counter;        %количество арифметических операций, произведенных в методе Ньютона
 end
 disp('   Решение ')
-    disp(xn)
-%disp('   Погрешность ')
-%disp(delta)
+disp(vpa(xn, 5))
+disp('   Погрешность ')
+disp(delta)
+disp('   Произведено арифметических операций')
+disp(globalCounter)
