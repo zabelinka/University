a87ce287561480f48bc3228a89c1b3aaac559b6e
Tatyana Zabelina <zzabelinka@gmail.com>
2015-06-20 23:09:01 +0400

    Added quicksort function for structure of the distances between points

--- CUDA Fingerprinting 2/Consol_Test_for_CUDA/Consol_Test_for_CUDA/main.cpp
+++ CUDA Fingerprinting 2/Consol_Test_for_CUDA/Consol_Test_for_CUDA/main.cpp
@@ -19,58 +19,26 @@ struct Dist{									// value keep dictance between point[i] and point[j]
 	double value;
 	int i;
 	int j;
-
-	void change(Dist another){					// replace the fields of object calling the function with the fields of another object
-		double temp_value = this->value;
-		int temp_i = this->i;
-		int temp_j = this->j;
-		this->value = another.value;
-		this->i = another.i;
-		this->j = another.j;
-		another.value = temp_value;
-		another.i = temp_i;
-		another.j = temp_j;
-	}
 };
 
 class Map{
 public:
-	Point* pointArray;							// array of points on the map
-	double** dist_matrix;									// array of distances between points
-	// not used
-	Dist* arrayDist;								// the array of distances between points
 	const double DELTA = 0.5;						// max deviation between the same points on the two maps
 	const int NUM = 5; // rand() % 40 + 60;			// the number of the points in range of 60 to 100
+	const int ARRAYDIST_SIZE = NUM * (NUM - 1) / 2;	// the number of edges in the complete graph
+	Point* pointArray;								// array of points on the map
+	Dist* arrayDist;								// the array of distances between points
 	
 	Map(){										// constructor. create a map of points
 		pointArray = new Point[NUM];
-		cout << "created an array of " << NUM << " points" << endl;
+		cout << "CREATED AN ARRAY OF " << NUM << " POINTS" << endl;
 		for (int i = 0; i < NUM; i++){			// for each point set coordinates
 			pointArray[i].setPoint(rand() % 100 + 1, rand() % 100 + 1);					
 		}
 	}
 
-	// not used
-	void distance_matrix(){								// fill matrix with distances between points
-		// create matrix of distances
-		dist_matrix = new double*[NUM];
-		for (int i = 0; i < NUM; i++){
-			dist_matrix[i] = new double[NUM];
-		}
-		// fill matrix with distances between points and print it
-		for (int i = 0; i < NUM; i++){
-			for (int j = 0; j < NUM; j++){
-				dist_matrix[i][j] = sqrt(										// Pythagorean theorem
-					pow(pointArray[i].x - pointArray[j].x, 2) + 
-					pow(pointArray[i].y - pointArray[j].y, 2));
-				cout << dist_matrix[i][j] << "\t";
-			}
-			cout << endl;
-		}
-	}
-
-	void distance(){									// fill Dist structure with distances between point[i] and point[j] for each i and j
-		arrayDist = new Dist[NUM * (NUM - 1) / 2];		// the number of edges in the complete graph
+	void getDistance(){									// fill Dist structure with distances between point[i] and point[j] for each i and j
+		arrayDist = new Dist[ARRAYDIST_SIZE];			// the number of edges in the complete graph
 		int k = 0;										// the index of the element
 		for (int i = NUM - 1; i > 0; i--){
 			for (int j = 0; j < i; j++){
@@ -91,24 +59,26 @@ public:
 	}
 };
 
-int partition(int* a, int low, int hight){						// partition of the array for quicksort
-	int l = low, r = hight;										// left and right indexes
-	int pivot = a[(low + hight) / 2];							// index of a pivot element in the middle of the array
+int partition(Dist* dist, int low, int hight){						// partition of the array for quicksort
+	int l = low, r = hight;											// left and right indexes
+	double pivot = dist[(low + hight) / 2].value;					// index of a pivot element in the middle of the array
 	while (true){
-		cout << "l <= r\t" << l << " <= " << r << endl;
-		while (a[l] < pivot) l++;
-		while (a[r] > pivot) r--;
-		if (l < r) swap(a[l], a[r]);
+		while (dist[l].value < pivot) l++;
+		while (dist[r].value > pivot) r--;
+		if (l < r) {
+			swap(dist[l].value, dist[r].value);
+			swap(dist[l].i, dist[r].i);
+			swap(dist[l].j, dist[r].j);
+		}
 		else return r;
 	}
 }
 
-void sort(int *a, int low, int hight){							// quicksort
-	cout << "sort " << low << " -- " << hight << endl;
+void sort(Dist *dist, int low, int hight){							// quicksort
 	if (low < hight){
-		int pivot = partition(a, low, hight);					// partition of the array: { < pivot | pivot | > pivot}
-		sort(a, low, pivot);									//		recursive call for 
-		sort(a, pivot + 1, hight);								//		two parts of the array
+		int pivot_index = partition(dist, low, hight);				// partition of the array: { < pivot | pivot | > pivot}
+		sort(dist, low, pivot_index);								//		recursive call for 
+		sort(dist, pivot_index + 1, hight);							//		two parts of the array
 	}
 }
 
@@ -121,17 +91,17 @@ int main(){
 	Map* secondMap = new Map();
 
 	// fill structure with distances between points
-	firstMap->distance();
-	firstMap->printDistances();							// show results
+	firstMap->getDistance();
+	firstMap->printDistances();							// show results: print structure of distances
 	cout << endl;
-	secondMap->distance();
-	secondMap->printDistances();							// show results
+	secondMap->getDistance();
+	secondMap->printDistances();						// show results: print structure of distances
 
-	int a[] = {6, 0, 1, 5, 7, 2, 3, 8, 4, 9};
-	sort(a, 0, 9);
-	for (int i = 0; i < 10; i++){
-		cout << a[i] << "\t";
-	}
+	cout << endl << "The distances of the second map after sorting" << endl;
+	sort(secondMap->arrayDist, 0, secondMap->ARRAYDIST_SIZE - 1);
+	secondMap->printDistances();						// show results: print structure of distances
+
+
 
 	
 
