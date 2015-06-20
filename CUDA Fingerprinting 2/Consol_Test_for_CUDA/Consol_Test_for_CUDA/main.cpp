245360214e73c77f93445531c5f5ba64acd0c36c
Tatyana Zabelina <zzabelinka@gmail.com>
2015-06-20 17:59:45 +0400

    Create matrix and fill it with distances between points

--- CUDA Fingerprinting 2/Consol_Test_for_CUDA/Consol_Test_for_CUDA/main.cpp
+++ CUDA Fingerprinting 2/Consol_Test_for_CUDA/Consol_Test_for_CUDA/main.cpp
@@ -18,9 +18,9 @@ struct  Point{									// point on the plane
 class Map{
 public:
 	Point* pointArray;							// array of points on the map
-	int* dist;									// array of distances between points
-	const int DELTA = 5;						// max deviation between the same points on the two maps
-	const int NUM = rand() % 40 + 60;			// the number of the points in range of 60 to 100
+	double** dist;									// array of distances between points
+	const double DELTA = 0.5;						// max deviation between the same points on the two maps
+	const int NUM = 2; // rand() % 40 + 60;			// the number of the points in range of 60 to 100
 	
 	Map(){										// constructor. create a map of points
 		pointArray = new Point[NUM];
@@ -30,6 +30,23 @@ public:
 		}
 	}
 
+	void distance(){
+		// create matrix of distances
+		dist = new double*[NUM];
+		for (int i = 0; i < NUM; i++){
+			dist[i] = new double[NUM];
+		}
+		// fill matrix with distances between points and print it
+		for (int i = 0; i < NUM; i++){
+			for (int j = 0; j < NUM; j++){
+				dist[i][j] = sqrt(										// Pythagorean theorem
+					pow(pointArray[i].x - pointArray[j].x, 2) + 
+					pow(pointArray[i].y - pointArray[j].y, 2));
+				cout << dist[i][j] << "\t";
+			}
+			cout << endl;
+		}
+	}
 };
 
 int main(){
@@ -40,7 +57,12 @@ int main(){
 	cout << endl;
 	Map* secondMap = new Map();
 
-	getchar();
+	// fill matrix with distances between points
+	firstMap->distance();						
+	secondMap->distance();
+
+
+	system("pause");
 	return 0;
 }
 
