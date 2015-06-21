134301b590a0a8256051b53bfc5fc5d2d1d3012f
Tatyana Zabelina <zzabelinka@gmail.com>
2015-06-22 01:59:25 +0400

    Add translation and turn. Smth works wrong

--- CUDA Fingerprinting 2/Consol_Test_for_CUDA/Consol_Test_for_CUDA/main.cpp
+++ CUDA Fingerprinting 2/Consol_Test_for_CUDA/Consol_Test_for_CUDA/main.cpp
@@ -1,18 +1,26 @@
+#define _USE_MATH_DEFINES 
 #include <iostream>
 #include "stdlib.h"
-#include "stdio.h"								// time allows to initialize random seed for the pseudo-random number generator algorithm
-#include <time.h> 
+#include "stdio.h"								
+#include <time.h>								// time allows to initialize random seed for the pseudo-random number generator algorithm
+#include <iomanip>								// for printing n symbols after point
+#include <math.h>								// for using const PI
 using namespace std;
 
 struct  Point{									// point on the plane
 	int  x;										// coordinates of the point
 	int  y;
 	
+	void printPoint(){							// print point coordinates by example (x, y)
+		cout << "(" << x << ", " << y << ")";
+	}
 	void setPoint(int x, int y){				// set coordinates x and y to the point
 		this->x = x;
 		this->y = y;
-		cout << "created point (" << x << ", " << y << ")" << endl;
+		this->printPoint();
+		cout << endl;
 	}
+	
 };
 
 struct Dist{									// value keep dictance between point[i] and point[j]
@@ -21,13 +29,14 @@ struct Dist{									// value keep dictance between point[i] and point[j]
 	int j;
 };
 
-int compare_exact(const void* a, const void* b){			// compare-function for quick sort of the Dist-structure array
+int compare_exact(const void* a, const void* b){			// compare-function by exact matching for quick sorting of the Dist-structure array
 	double a_value = ((const struct Dist*)a)->value;		// conver void* to Dist* and select the field "value" for comparison
 	double b_value = ((const struct Dist*)b)->value;
 
 	if (a_value < b_value) return -1;						// comparison
 	if (a_value > b_value) return 1;
-	if (a_value == b_value) return 0;
+	// if (a_value == b_value) 
+	return 0;
 }
 
 int binarySearch(Dist* dist, int low, int hight, double key, const double DELTA){		// find the distance in ordered array dist which absolute value differs from the key less then on DELTA
@@ -56,7 +65,7 @@ public:
 		pointArray = new Point[NUM];
 		cout << "CREATED AN ARRAY OF " << NUM << " POINTS" << endl;
 		for (int i = 0; i < NUM; i++){			// for each point set coordinates
-			pointArray[i].setPoint(rand() % 100 + 1, rand() % 100 + 1);					
+			pointArray[i].setPoint(rand() % 1000 + 1, rand() % 1000 + 1);					
 		}
 	}
 
@@ -81,14 +90,100 @@ public:
 		}
 	}
 
-	void search(Dist *orderedDist, int orderedDistSize, const double DELTA){
-		for (int i = 0; i < this->ARRAYDIST_SIZE; i++){
+	// calculates parameters of translation of the second map concerning the first on pivotPoints and makes translation for all points of the array of the second map
+	void translation(Point pivotFirst, Point pivotSecond, Point* arrayPoint, const int arrayPointSize){
+		int dx = pivotFirst.x - pivotSecond.x;		
+		int dy = pivotFirst.y - pivotSecond.y;
+
+		for (int i = 0; i < arrayPointSize; i++){
+			cout << i << "\t";
+			arrayPoint[i].printPoint();
+			cout << "\ttransfers to\t";
+			arrayPoint[i].x += dx;
+			arrayPoint[i].y += dy;
+			arrayPoint[i].printPoint();
+			cout << endl;
+		}
+	}
+
+	// calculates the angle of turn of points of the second map around the pole and makes turn for all points of the array of the second map
+	void turn(Point pole, Point X, Point Y, Point* arrayPoint, const int arrayPointSize){
+		// find the angle between X1X2 and X1Y2
+		double dy = X.y - pole.y;
+		double dx = X.x - pole.x;
+		double slope1 = dy / dx;
+		dy = Y.y - pole.y;
+		dx = Y.x - pole.x;
+		double slope2 = dy / dx;
+		double angle;
+		if (slope1 * slope2 == -1){
+			angle = M_PI_2;
+		}
+		else{
+			angle = -atan((slope2 - slope1) / (1 + slope1 * slope2));
+		}
+
+		for (int i = 0; i < arrayPointSize; i++){
+			if (i == 54){
+				cout << 54;
+			}
+			cout << i << "\t";
+			arrayPoint[i].printPoint();
+			cout << "\tturns to\t";
+
+			int x = arrayPoint[i].x;
+			int y = arrayPoint[i].y;
+			arrayPoint[i].x = (x - pole.x) * cos(angle) - (y - pole.y) * sin(angle) + pole.x;
+
+			arrayPoint[i].y = (x - pole.x) * sin(angle) + (y - pole.y) * cos(angle) + pole.y;
+
+			arrayPoint[i].printPoint();
+			cout << endl;
+		}
+	}
+
+	void search(Map secondMap, const double DELTA){				// find the semelar distances in two maps. The second map must have an ordered array of distances.
+		Dist* orderedDist = secondMap.arrayDist;				// ordered array of the distances
+		int orderedDistSize = secondMap.ARRAYDIST_SIZE;			// length of the ordered array of the distances
+
+		for (int k = 0; k < this->ARRAYDIST_SIZE; k++){
 			// the checked value must belong to the interval between min and max values of the orderedDist
-			if (this->arrayDist[i].value > orderedDist[0].value - DELTA &&
-				this->arrayDist[i].value < orderedDist[orderedDistSize - 1].value + DELTA){
-				int found_index = binarySearch(orderedDist, 0, orderedDistSize - 1, this->arrayDist[i].value, DELTA);
-				if (found_index != -1){
-					cout << "!!! Found " << this->arrayDist[i].value << " --> " << orderedDist[found_index].value << endl;
+			if (this->arrayDist[k].value > orderedDist[0].value - DELTA &&
+				this->arrayDist[k].value < orderedDist[orderedDistSize - 1].value + DELTA)
+			{
+				int found_index = binarySearch(orderedDist, 0, orderedDistSize - 1, this->arrayDist[k].value, DELTA);		// find value of arrayDist[k] in orderedDist
+				if (found_index != -1){			// if it is found
+					// print two similar distances
+					cout << fixed << setprecision(15) << "Found " << this->arrayDist[k].value << " --> " << orderedDist[found_index].value << endl;
+					// pivot points
+					// first map
+					Point X1 = this->pointArray[this->arrayDist[k].i];
+					Point X2 = this->pointArray[this->arrayDist[k].j];
+					// second map
+					Point Y1 = secondMap.pointArray[orderedDist[found_index].i];
+					Point Y2 = secondMap.pointArray[orderedDist[found_index].j];
+					
+					// print points with these dastances
+					cout << "First map" << endl;
+						cout << this->arrayDist[k].value << " -- distance between ";
+						X1.printPoint();
+						cout << " and ";
+						X2.printPoint();
+						cout << endl;
+					cout << "Second map" << endl;
+						cout << orderedDist[found_index].value << " -- distance between ";
+						Y1.printPoint();
+						cout << " and ";
+						Y2.printPoint();
+						cout << endl;
+
+					// translation and turn
+					translation(X1, Y1, secondMap.pointArray, secondMap.NUM);
+					cout << endl;
+					// update pivot points of the second map
+					Y2 = secondMap.pointArray[orderedDist[found_index].j];
+					turn(X1, X2, Y2, secondMap.pointArray, secondMap.NUM);
+					break;			// after one matching finish searching
 				}
 			}
 			else{
@@ -102,8 +197,8 @@ public:
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(){
-	srand(time(NULL));							// initialize random seed -- an integer value to be used by the pseudo-random number generator algorithm
-	const double DELTA = 1.0e-10;						// max deviation between the same points on the two maps
+	//srand(time(NULL));							// initialize random seed -- an integer value to be used by the pseudo-random number generator algorithm
+	const double DELTA = 1e-20;						// max deviation between the same points on the two maps
 
 	// create maps
 	Map* firstMap = new Map();
@@ -124,9 +219,8 @@ int main(){
 	
 	// search the distance of the first map in the second map
 	cout << endl << "Search with DELTA = " << DELTA << endl;
-	firstMap->search(secondMap->arrayDist, secondMap->ARRAYDIST_SIZE, DELTA);
+	firstMap->search(*secondMap, DELTA);
 
-	
 	
 	system("pause");
 	return 0;
