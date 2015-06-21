f64c9f5fbc5a77c5835d1c1dfe5047bd5b200b2d
Tatyana Zabelina <zzabelinka@gmail.com>
2015-06-21 18:43:06 +0400

    Using standard qsort function. Fixed binarySearch function

--- CUDA Fingerprinting 2/Consol_Test_for_CUDA/Consol_Test_for_CUDA/main.cpp
+++ CUDA Fingerprinting 2/Consol_Test_for_CUDA/Consol_Test_for_CUDA/main.cpp
@@ -21,45 +21,33 @@ struct Dist{									// value keep dictance between point[i] and point[j]
 	int j;
 };
 
-int partition(Dist* dist, int low, int hight){						// partition of the array for quicksort
-	int l = low, r = hight;											// left and right indexes
-	double pivot = dist[(low + hight) / 2].value;					// index of a pivot element in the middle of the array
-	while (true){
-		while (dist[l].value < pivot) l++;
-		while (dist[r].value > pivot) r--;
-		if (l < r) {
-			swap(dist[l].value, dist[r].value);
-			swap(dist[l].i, dist[r].i);
-			swap(dist[l].j, dist[r].j);
-		}
-		else return r;
-	}
-}
+int compare_exact(const void* a, const void* b){			// compare-function for quick sort of the Dist-structure array
+	double a_value = ((const struct Dist*)a)->value;		// conver void* to Dist* and select the field "value" for comparison
+	double b_value = ((const struct Dist*)b)->value;
 
-void sort(Dist *dist, int low, int hight){							// quicksort
-	if (low < hight){
-		int pivot_index = partition(dist, low, hight);				// partition of the array: { < pivot | pivot | > pivot}
-		sort(dist, low, pivot_index);								//		recursive call for 
-		sort(dist, pivot_index + 1, hight);							//		two parts of the array
-	}
+	if (a_value < b_value) return -1;						// comparison
+	if (a_value > b_value) return 1;
+	if (a_value == b_value) return 0;
 }
 
 int binarySearch(Dist* dist, int low, int hight, double key, const double DELTA){		// find the distance in ordered array dist which absolute value differs from the key less then on DELTA
 	int index = (low + hight) / 2;
 	// cout << "abs(dist[index].value - key) = " << abs(dist[index].value - key) << endl;
-	if (abs(dist[index].value - key) < DELTA) return index;					// found
-	if (low == hight || low > hight) return -1;											// not found
-	if (key < dist[index].value)
-		return binarySearch(dist, low, index - 1, key, DELTA);				// go to the left
-	if (key > dist[index].value)
-		return binarySearch(dist, index + 1, hight, key, DELTA);			// go to the right
+	while (low <= hight){
+		if (abs(dist[index].value - key) < DELTA) return index;					// found
+		if (key < dist[index].value)
+			return binarySearch(dist, low, index - 1, key, DELTA);				// go to the left
+		if (key > dist[index].value)
+			return binarySearch(dist, index + 1, hight, key, DELTA);			// go to the right
+	}
+	return -1;																	// not found
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Map{
 public:
-	const int NUM = 5; // rand() % 40 + 60;			// the number of the points in range of 60 to 100
+	const int NUM = rand() % 40 + 60;				// the number of the points in range of 60 to 100
 	const int ARRAYDIST_SIZE = NUM * (NUM - 1) / 2;	// the number of edges in the complete graph
 	Point* pointArray;								// array of points on the map
 	Dist* arrayDist;								// the array of distances between points
@@ -104,10 +92,10 @@ public:
 				}
 			}
 			else{
-				cout << "The value " << this->arrayDist[i].value << " doesn't belong to the necessary interval." << endl;
+				// cout << "The value " << this->arrayDist[i].value << " doesn't belong to the necessary interval." << endl;
 			}
 		}
-		
+
 	}
 };
 
@@ -115,32 +103,31 @@ public:
 
 int main(){
 	srand(time(NULL));							// initialize random seed -- an integer value to be used by the pseudo-random number generator algorithm
-	const double DELTA = 0.5;						// max deviation between the same points on the two maps
+	const double DELTA = 1.0e-10;						// max deviation between the same points on the two maps
 
-	// create 2 maps
+	// create maps
 	Map* firstMap = new Map();
 	cout << endl;
 	Map* secondMap = new Map();
 
 	// fill structure with distances between points
 	firstMap->getDistance();
-	firstMap->printDistances();							// show results: print structure of distances
+	//firstMap->printDistances();							// show results: print structure of distances
 	cout << endl;
 	secondMap->getDistance();
-	secondMap->printDistances();						// show results: print structure of distances
+	//secondMap->printDistances();						// show results: print structure of distances
 
-	// sort the distance values only for the second map
-	cout << endl << "The distances of the second map after sorting" << endl;
-	sort(secondMap->arrayDist, 0, secondMap->ARRAYDIST_SIZE - 1);
-	secondMap->printDistances();						// show results: print structure of distances
-
+	// qsort for the distance values only for the second map
+	qsort(secondMap->arrayDist, secondMap->ARRAYDIST_SIZE, sizeof(secondMap->arrayDist[0]), compare_exact);		
+	//cout << endl << "The distances of the second map after sorting" << endl;
+	//secondMap->printDistances();						// show results: print structure of distances
+	
 	// search the distance of the first map in the second map
-
-
-	//cout << binarySearch(secondMap->arrayDist, 0, secondMap->ARRAYDIST_SIZE - 1, firstMap->arrayDist[0].value, DELTA);
+	cout << endl << "Search with DELTA = " << DELTA << endl;
 	firstMap->search(secondMap->arrayDist, secondMap->ARRAYDIST_SIZE, DELTA);
 
+	
+	
 	system("pause");
 	return 0;
 }
-
